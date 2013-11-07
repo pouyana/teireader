d89d797d1debee42a71a7da57557fbf82c5be189
Pouyan <popeno2003@gmail.com>
2013-11-07 15:43:51 +0100

    some small chnages

---
+++ drama.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+class Drama:
+	"""Drama Self Class, which contain description and common data of a drama file."""
+	def __init__(self, id,title ):
+		self.id = id
+		self.title = title
--- parser.py
+++ parser.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import xml.etree.ElementTree as ET
+from drama import Drama
 
 tree = ET.parse('files/LessiMinna_von_BaDrame.xml')
 root = tree.getroot()
-print root.tag
+id = root.attrib['{http://www.w3.org/XML/1998/namespace}id']
+name = root.attrib['n']
+drama = Drama(id,name)
+print drama.title
