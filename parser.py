0e61fc5a973c05101be264148d01508a42420bc2
Pouyan <popeno2003@gmail.com>
2013-11-07 15:26:59 +0100

    added guildlines for TEI data.some small test

--- parser.py
+++ parser.py
@@ -1,2 +1,7 @@
-#!#/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('files/LessiMinna_von_BaDrame.xml')
+root = tree.getroot()
+print root.tag
