333f73c1a5e49f2f71e62e7f7d3c6099fae3866a
Pouyan Azari <popeno2003@gmail.com>
2013-11-12 16:59:36 +0100

    added license

--- parser.py
+++ parser.py
@@ -2,4 +2,8 @@
 # -*- coding: utf-8 -*-
 from drama import Drama
 the_drama =  Drama('files/LessiMinna_von_BaDrame.xml')
+#test the file properties.
 print the_drama.get_title()
+print the_drama.get_bibl_title()
+print the_drama.get_bibl_author()
+print the_drama.get_license()
