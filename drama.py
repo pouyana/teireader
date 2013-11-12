f5de4904cb21c614e400902030cd71c86ab74af8
Pouyan <popeno2003@gmail.com>
2013-11-12 16:16:30 +0100

    some new object oriented stuff

--- drama.py
+++ drama.py
@@ -1,7 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#the code has extentive use of xPath.
+import xml.etree.ElementTree as ET
 class Drama:
+	#root = None
 	"""Drama Self Class, which contain description and common data of a drama file."""
-	def __init__(self, id,title ):
-		self.id = id
-		self.title = title
+	prefix = "{http://www.tei-c.org/ns/1.0}"
+	def __init__(self,text_file):
+		self.text_file = text_file
+		self.import_xml()
+
+	def import_xml(self):
+		tree = ET.parse(self.text_file)
+		root = tree.getroot()
+		self.set_root(root)
+	
+	def set_root(self,root):
+		self.root = root
+	
+	def get_root(self):
+		return self.root
+
+	def get_all_parent(self,element):
+		parent = self.root.findall(".//"+element.tag+"/..")
+		return parent
+
+	def get_parent(self,element):
+		parent = self.root.find(".//"+element.tag+"/..")
+		return parent
+
+	def get_first_child(self,element):
+		child = self.root.find(".//"+element.tag+"/*")
+		return child
+	
+	def get_all_child(self,element):
+		child = self.root.findall(".//"+element.tag+"/*")
+		return child
+	#search a list for a tag
+	def find_by_tag(self,lst,tag):
+		for element in lst:
+			if(hasattr(element,"tag")):
+				if(element.tag==tag):
+					return element
+	#get book title from the header files.
+	def get_title(self):
+		fDs = self.root.findall(".//"+self.prefix+"fileDesc")
+		for fD in fDs:
+			titleStmt = self.find_by_tag(fD,self.prefix+"titleStmt")
+			return self.get_child(titleStmt).text
+
+
--- parser.py
+++ parser.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import xml.etree.ElementTree as ET
 from drama import Drama
-
-tree = ET.parse('files/LessiMinna_von_BaDrame.xml')
-root = tree.getroot()
-id = root.attrib['{http://www.w3.org/XML/1998/namespace}id']
-name = root.attrib['n']
-drama = Drama(id,name)
-print drama.title
+the_drama =  Drama('files/LessiMinna_von_BaDrame.xml')
+print the_drama.get_title()
