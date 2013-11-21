a6002cb3c9d264cf70fdbc3768c338d0d54ccf1b
Pouyan <popeno2003@gmail.com>
2013-11-21 14:25:17 +0100

    added get cast

--- parser.py
+++ parser.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from drama import Drama
-the_drama =  Drama('files/LessiMinna_von_BaDrame.xml')
+the_drama =  Drama('files/stranitzky_tuerk.xml')
 #test the file properties.
 print the_drama.get_title()
 print the_drama.get_bibl_title()
 print the_drama.get_bibl_author()
 print the_drama.get_publish_data()["license"]
 print the_drama.get_creation_date()
+print the_drama.get_cast()
