29ea8ade0b51ec08fb0fac9e2e18deac0bb71031
Pouyan Azari <popeno2003@gmail.com>
2013-12-04 17:23:02 +0100

    added multiupload https://bitbucket.org/xavrenard/multiupload_module/

--- web-ui/web2py/applications/grid/controllers/default.py
+++ web-ui/web2py/applications/grid/controllers/default.py
@@ -76,11 +76,48 @@ def data():
     """
     return dict(form=crud())
 
-def first():
-    if request.vars.visitor_name:
-        session.visitor_name = request.vars.visitor_name
-        redirect(URL('second'))
-    return dict()
+def upload_file():
+        """
+        File upload handler for the ajax form of the plugin jquery-file-upload
+        Return the response in JSON required by the plugin
+        """
+        try:
+            # Get the file from the form
+            f = request.vars['files[]']
+            
+            # Store file
+            id = db.files.insert(doc = db.files.doc.store(f.file, f.filename))
+            
+            # Compute size of the file and update the record
+            record = db.files[id]
+            path_list = []
+            path_list.append(request.folder)
+            path_list.append('uploads')
+            path_list.append(record['doc'])
+            size =  shutil.os.path.getsize(shutil.os.path.join(*path_list))
+            File = db(db.files.id==id).select()[0]
+            db.files[id] = dict(sizeFile=size)
+            db.files[id] = dict(sessionId=response.session_id)
+            
+            res = dict(files=[{"name": str(f.filename), "size": size, "url": URL(f='download', args=[File['doc']]), "delete_url": URL(f='delete_file', args=[File['doc']]), "delete_type": "DELETE" }])
+            
+            return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
 
-def second():
-    return dict()
+        except:
+            return dict(message=T('Upload error'))
+
+
+def delete_file():
+        """
+        Delete an uploaded file
+        """
+        try:
+            name = request.args[0]
+            db(db.files.doc==name).delete()
+            return dict(message=T('File deleted'))
+        except:
+            return dict(message=T('Deletion error'))
+
+
+def upload():
+        return dict()
---
+++ web-ui/web2py/applications/grid/static/jQuery-File-Upload/js/main.js
@@ -0,0 +1,51 @@
+/*
+ * jQuery File Upload Plugin JS Example 7.0
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+/*jslint nomen: true, unparam: true, regexp: true */
+/*global $, window, document */
+
+$(function () {
+    'use strict';
+
+    // Initialize the jQuery File Upload widget:
+    $('#fileupload').fileupload({
+        // Uncomment the following to send cross-domain cookies:
+        //xhrFields: {withCredentials: true},
+        url: 'http://127.0.0.1:8080/grid/default/upload_file',
+        autoUpload: 'True',
+        maxNumberOfFiles: 3,
+        maxFileSize: 2500000,
+        //acceptFileTypes: '/(\.|\/)(gif|jpe?g|png)$/i',
+    });
+
+    // Enable iframe cross-domain access via redirect option:
+    $('#fileupload').fileupload(
+        'option',
+        'redirect',
+        window.location.href.replace(
+            /\/[^\/]*$/,
+            '/cors/result.html?%s'
+        )
+    );
+
+	// Load existing files:
+	$.ajax({
+		// Uncomment the following to send cross-domain cookies:
+		//xhrFields: {withCredentials: true},
+		url: $('#fileupload').fileupload('option', 'url'),
+		dataType: 'json',
+		context: $('#fileupload')[0]
+	}).done(function (result) {
+		$(this).fileupload('option', 'done')
+			.call(this, null, {result: result});
+	});
+
+});
