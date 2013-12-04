75825afb7bdff233eb81b57f9a8d9ee2edf4ef84
Pouyan Azari <popeno2003@gmail.com>
2013-12-04 17:56:34 +0100

    added some files

--- web-ui/web2py/applications/grid/controllers/default.py
+++ web-ui/web2py/applications/grid/controllers/default.py
@@ -84,27 +84,14 @@ def upload_file():
         try:
             # Get the file from the form
             f = request.vars['files[]']
-            
             # Store file
             id = db.files.insert(doc = db.files.doc.store(f.file, f.filename))
-            
             # Compute size of the file and update the record
-            record = db.files[id]
-            path_list = []
-            path_list.append(request.folder)
-            path_list.append('uploads')
-            path_list.append(record['doc'])
-            size =  shutil.os.path.getsize(shutil.os.path.join(*path_list))
-            File = db(db.files.id==id).select()[0]
-            db.files[id] = dict(sizeFile=size)
-            db.files[id] = dict(sessionId=response.session_id)
-            
-            res = dict(files=[{"name": str(f.filename), "size": size, "url": URL(f='download', args=[File['doc']]), "delete_url": URL(f='delete_file', args=[File['doc']]), "delete_type": "DELETE" }])
-            
+            res = dict(files=[{"name": str(f.filename), "url": URL(f='download', args=[File['doc']]), "delete_url": URL(f='delete_file', args=[File['doc']]), "delete_type": "DELETE" }])
             return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
 
         except:
-            return dict(message=T('Upload error'))
+            return 'Upload error'+ "".join(request.vars)
 
 
 def delete_file():
