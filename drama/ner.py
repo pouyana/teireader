23442bb37af2132efa8ae76ee2f2f1a263e640af
Pouyan Azari <popeno2003@gmail.com>
2014-02-27 19:40:37 +0100

    make class

--- drama/ner.py
+++ drama/ner.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
-import nltk.data
 from nltk import sent_tokenize, word_tokenize, pos_tag, ne_chunk, FreqDist
-import pickle
 import re
 from drama import Drama
+class Ner:
+    def extract_entities(self,text):
+        entities = []
+        words = word_tokenize(text)
+        chunks = ne_chunk(pos_tag(words))
+        entities.extend([chunk for chunk in chunks if hasattr(chunk, 'node')])
+        return entities
 
-def extract_entities(text):
-    entities = []
-    words = word_tokenize(text)
-    chunks = ne_chunk(pos_tag(words))
-    entities.extend([chunk for chunk in chunks if hasattr(chunk, 'node')])
-    return entities
-
-text1="Selenissa Horribilicribrifax Harpax"
-
+    def all_names(self,text):
+        words = re.findall("\w+",text)
+        normalized_string = text.replace(".","")
+        entities = self.extract_entities(normalized_string)
+        name_list =[]
+        for entitiy in entities:
+            for name in entitiy:
+                name_list.append(name[0])
+        if (set(words)==set(name_list)):
+            return True
+#test case
 text = Drama("files/Horribilicribrifax.xml")
+ner = Ner()
 collection_a = text.get_fix_stage()
 for c in collection_a:
         if("text" in c):
@@ -22,20 +30,5 @@ for c in collection_a:
             stage["text"]=c["text"]
             stage["id"]=c["id"]
             if(c["text"]):
-		#find all the words in the sentence
-		text_words = re.findall("\w+",c["text"])
-		#print text_words
-		#named entity recognition 
-		#there is problem with dots so I remove them.
-		#I know that I send only on sentence to the entity recognizer
-		normilized_string = c["text"].replace(".","")
-		entities = extract_entities(normilized_string)
-		#print entities
-		name_list = []
-		for entitiy in entities:
-			for names in entitiy:
-				name_list.append(names[0])
-		#see ordered list / unodered lists.
-		#print c["text"]
-		if (set(text_words)==set(name_list)):
-			print c["text"]
+                if(ner.all_names(c["text"])):
+                    print c["text"]
