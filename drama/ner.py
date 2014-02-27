fdd7323d888cb2ca7724b7ebe8d85325073c8324
Pouyan Azari <popeno2003@gmail.com>
2014-02-27 17:35:08 +0100

    debug errors with named entity recognition

--- drama/ner.py
+++ drama/ner.py
@@ -12,7 +12,9 @@ def extract_entities(text):
     entities.extend([chunk for chunk in chunks if hasattr(chunk, 'node')])
     return entities
 
-text = Drama("ncfiles/WeiseMasanielloDrame.xml")
+text1="Selenissa Horribilicribrifax Harpax"
+
+text = Drama("files/Horribilicribrifax.xml")
 collection_a = text.get_fix_stage()
 for c in collection_a:
         if("text" in c):
@@ -22,11 +24,18 @@ for c in collection_a:
             if(c["text"]):
 		#find all the words in the sentence
 		text_words = re.findall("\w+",c["text"])
-		#named entity recognition
-		entities = extract_entities(c["text"].rstrip('\r\n'))
+		#print text_words
+		#named entity recognition 
+		#there is problem with dots so I remove them.
+		#I know that I send only on sentence to the entity recognizer
+		normilized_string = c["text"].replace(".","")
+		entities = extract_entities(normilized_string)
+		#print entities
 		name_list = []
 		for entitiy in entities:
-			name_list.append(entitiy[0][0])
+			for names in entitiy:
+				name_list.append(names[0])
 		#see ordered list / unodered lists.
+		#print c["text"]
 		if (set(text_words)==set(name_list)):
 			print c["text"]
